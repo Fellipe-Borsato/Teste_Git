885416266ebd5266c8eeba7ef02e32e6c0abf31d
Fellipe-Borsato <fellipeborsato@gmail.com>
2024-03-06 21:31:52 -0300

    +nome fellipe
    adicionei meu nome

--- main.cs
+++ main.cs
@@ -3,7 +3,9 @@ class Program {
   public static void Main (string[] args) {
 
     string Nome = "YASMIN";
+    string Nomef= "Fellipe";
+
     Console.WriteLine ($"{Nome}!");
-    
+    Console.WriteLine ($"{Nomef}!");
   }
 }
